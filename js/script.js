5bbb74b934a12e778b0d83470ae232fcd7dc72a9
Yago Nieto Garrido <nietogarridoyago@gmail.com>
2022-09-26 10:04:27 +0200

    Practica 1 unidad 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Arrancando la página");
+if(true){
+    let a = "hola";
+    alert(a);
+}
+
+let nombre = "Yago";
+let declaracion = `Hola me llamo ${nombre}`; // "Hola me llamo " + a; es igual. Mas comodo de la forma primera
+console.log(declaracion);
+console.log(1+2+"3"+4+5);
